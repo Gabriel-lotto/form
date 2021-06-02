2ca4b7ad2b60a0f801b1ec5f7a34466222ffcaf1
Gabriel <gabriel.nascimento@dcomp.ufs.br>
2021-06-02 18:01:04 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,94 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Cadastro /> -->
+    <form action="#" method="post">
+      <fieldset>
+        <fieldset class="grupo">
+          <div class="campo">
+            <label for="MatriculaEstudante">Matricula estudante: </label>
+            <input
+              type="text"
+              id="MatriculaEstudante"
+              name="MatriculaEstudante"
+              style="width: 10em"
+              value=""
+            />
+          </div>
+          <div class="campo">
+            <label for="NomeEstudante">Nome Estudante: </label>
+            <input
+              type="text"
+              id="NomeEstudante"
+              name="NomeEstudante"
+              style="width: 10em"
+              value=""
+            />
+          </div>
+        </fieldset>
+        <div class="campo">
+          <label for="email">Nome Cliente</label>
+          <input
+            type="text"
+            id="NomeCliente"
+            name="NomeCliente"
+            style="width: 20em"
+            value=""
+          />
+        </div>
+        <div class="campo">
+          <label for="EndereçoCliente">Endereço cliente</label>
+          <input
+            type="text"
+            id="EndereçoCliente"
+            name="EndereçoClientee"
+            style="width: 10em"
+            value=""
+          />
+        </div>
+
+        <fieldset>
+          <div>
+            <label for="Estado">Estado: </label>
+            <div>
+              <select>
+                <option v-for="(estado, index) in estados" :key="index.id">{{
+                  estado.sigla
+                }}</option>
+              </select>
+            </div>
+          </div>
+          <!-- <div class="campo">
+            <label for="Cidade">Cidade</label>
+            <select name="Cidade" id="Cidade">
+              <option value="">-{{}}</option>
+              <option value="PR">P</option>
+            </select>
+          </div> -->
+        </fieldset>
+        <button type="submit" name="submit">Cadastrar</button>
+      </fieldset>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      estados: [],
+    };
+  },
+  created: function() {
+    axios
+      .get("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+      .then((res) => {
+        this.estados = res.data;
+        console.log(this.estados);
+      });
+  },
+};
 </script>
 
 <style>
@@ -26,3 +101,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+// v-for="(estado, index) in estados" :key="index.id"
